14c39d345b19c635cb6327c1553cdfb61ad483f9
Rasmus Bach Jørgensen <rasm74e5@edu.eal.dk>
2019-02-20 13:58:49 +0100

    Add project files.

---
+++ KvalitetProjekt/Domain/Costumor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KvalitetProjekt.Domain
+{
+    public class Costumor
+    {
+        public string Id
+        {
+            get;
+            set;
+        }
+        public string Name
+        {
+            get;
+            set;
+        }
+        public string Adress
+        {
+            get;
+            set;
+        }
+        public string Zip
+        {
+            get;
+            set;
+        }
+        public string Town
+        {
+            get;
+            set;
+        }
+        public string Telephone
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ KvalitetProjekt/Domain/Order.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KvalitetProjekt.Domain
+{
+    public class Order
+    {
+        private string OrderLines
+        {
+            get;
+            set;
+        }
+        public string Costumer
+        {
+            get;
+            set;
+        }
+        public string OrderID
+        {
+            get;
+            set;
+        }
+        public string OrderDate
+        {
+            get;
+            set;
+        }
+        public string DeliveryDate
+        {
+            get;
+            set;
+        }
+        public string Picked
+        {
+            get;
+            set;
+        }
+        public AddOrder(string product, string quantity, string price)
+        {
+
+        }
+
+    }
+
+
+}
