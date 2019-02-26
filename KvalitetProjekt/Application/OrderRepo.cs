c35295326c97941e9cc4bcf4995a639ad3f7ed43
Rasmus Bach Jørgensen <rasm74e5@edu.eal.dk>
2019-02-26 12:40:31 +0100

    Jeg har lavet connectionstring til Mortens Database

--- KvalitetProjekt/Application/OrderRepo.cs
+++ KvalitetProjekt/Application/OrderRepo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,19 @@ namespace KvalitetProjekt.Application
 {
     class OrderRepo
     {
+        private static string connectionString =
+    "Server=EALSQL1.eal.local; Database= C_DB13_2018; User Id=C_STUDENT13; Password=C_OPENDB13;";
+        public void ConnectToUserDatabase()
+        {
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    con.Open();
+
+                }
+            }
+        }
     }
 }
